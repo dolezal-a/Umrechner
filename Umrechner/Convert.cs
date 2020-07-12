dff417a9493e708232274b847ccb4686d86b9af3
Andreas Dolezal <dolezal.andreas@outlook.at>
2020-07-12 22:37:37 +0200

    Initial Commit

---
+++ Umrechner/Convert.cs
@@ -0,0 +1,112 @@
+﻿namespace Umrechner
+{
+    public static class Convert
+    {
+        public static decimal Temperature(decimal value, Temperature from, Temperature to)
+        {
+            decimal isoValue = 0.0m;
+            decimal result = 0.0m;
+
+            if (from == to) return value;
+            if (from == Umrechner.Temperature.Kelvin) isoValue = value;
+            if (from == Umrechner.Temperature.Celsius) isoValue = value + 273.15m;
+            if (from == Umrechner.Temperature.Fahrenheit) isoValue = (value - 32) * 5 / 9 + 273.15m;
+
+            if (to == Umrechner.Temperature.Kelvin) result = isoValue;
+            if (to == Umrechner.Temperature.Celsius) result = isoValue - 273.15m;
+            if (to == Umrechner.Temperature.Fahrenheit) result = (isoValue - 273.15m) * 9 / 5 + 32;
+
+            return result;
+        }
+
+
+        public static decimal Distance(decimal value, Distance from, Distance to)
+        {
+            decimal isoValue = 0.0m;
+            decimal impValue = 0.0m;
+            decimal result = 0.0m;
+            bool impToIso = false;
+
+            if (from == to) return value;
+            if (from == Umrechner.Distance.Meter)
+            {
+                isoValue = value;
+            }
+            if (from == Umrechner.Distance.Centimeter)
+            {
+                isoValue = value / 100;
+            }
+            if (from == Umrechner.Distance.Millimeter)
+            {
+                isoValue = value / 1000;
+            }
+            if (from == Umrechner.Distance.Kilometer)
+            {
+                isoValue = value * 1000;
+            }
+            if( from == Umrechner.Distance.Inch)
+            {
+                impToIso = true;
+                impValue = value;
+            }
+            if (from == Umrechner.Distance.Feet)
+            {
+                impToIso = true;
+                impValue = value * 12;
+            }
+            if( from == Umrechner.Distance.Yard)
+            {
+                impToIso = true;
+                impValue = value * 36;
+            }
+            if( from == Umrechner.Distance.Mile)
+            {
+                impToIso = true;
+                impValue = value * 63360;
+            }
+
+
+            if (impToIso)
+            {
+                isoValue = impValue / 39.3701m;
+            }
+            else
+            {
+                impValue = isoValue * 39.3701m;
+            }
+
+
+            if (to == Umrechner.Distance.Meter) result = isoValue;
+            if (to == Umrechner.Distance.Centimeter) result = isoValue * 100;
+            if (to == Umrechner.Distance.Millimeter) result = isoValue * 1000;
+            if (to == Umrechner.Distance.Kilometer) result = isoValue / 1000;
+            if (to == Umrechner.Distance.Inch) result = impValue;
+            if (to == Umrechner.Distance.Feet) result = impValue / 12;
+            if (to == Umrechner.Distance.Yard) result = impValue / 36;
+            if (to == Umrechner.Distance.Mile) result = impValue / 63360;
+
+            return result;
+        }
+    }
+
+
+    public enum Temperature
+    {
+        Kelvin,
+        Celsius,
+        Fahrenheit
+    }
+
+
+    public enum Distance
+    {
+        Millimeter,
+        Centimeter,
+        Meter,
+        Kilometer,
+        Inch,
+        Feet,
+        Yard,
+        Mile
+    }
+}
---
+++ Umrechner/MainWindow.xaml.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Umrechner
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void CalculateDistance(object sender, KeyEventArgs e)
+        {
+            TextBox obj = sender as TextBox;
+            decimal value = 0.0m;
+
+            TxBlMessageBlock.Text = String.Empty;
+            if (sender != TbMillimeter) TbMillimeter.Text = String.Empty;
+            if (sender != TbCentimeter) TbCentimeter.Text = String.Empty;
+            if (sender != TbMeter) TbMeter.Text = String.Empty;
+            if (sender != TbKilometer) TbKilometer.Text = String.Empty;
+            if (sender != TbInch) TbInch.Text = String.Empty;
+            if (sender != TbFeet) TbFeet.Text = String.Empty;
+            if (sender != TbYard) TbYard.Text = String.Empty;
+            if (sender != TbMile) TbMile.Text = String.Empty;
+
+            if(decimal.TryParse(obj.Text, out value))
+            {
+                if(sender == TbMillimeter)
+                {
+                    TbCentimeter.Text = Convert.Distance(value, Distance.Millimeter, Distance.Centimeter).ToString("F4");
+                    TbMeter.Text = Convert.Distance(value, Distance.Millimeter, Distance.Meter).ToString("F4");
+                    TbKilometer.Text = Convert.Distance(value, Distance.Millimeter, Distance.Kilometer).ToString("F4");
+                    TbInch.Text = Convert.Distance(value, Distance.Millimeter, Distance.Inch).ToString("F4");
+                    TbFeet.Text = Convert.Distance(value, Distance.Millimeter, Distance.Feet).ToString("F4");
+                    TbYard.Text = Convert.Distance(value, Distance.Millimeter, Distance.Yard).ToString("F4");
+                    TbMile.Text = Convert.Distance(value, Distance.Millimeter, Distance.Mile).ToString("F4");
+                }
+                if (sender == TbCentimeter)
+                {
+                    TbMillimeter.Text = Convert.Distance(value, Distance.Centimeter, Distance.Millimeter).ToString("F4");
+                    TbMeter.Text = Convert.Distance(value, Distance.Centimeter, Distance.Meter).ToString("F4");
+                    TbKilometer.Text = Convert.Distance(value, Distance.Centimeter, Distance.Kilometer).ToString("F4");
+                    TbInch.Text = Convert.Distance(value, Distance.Centimeter, Distance.Inch).ToString("F4");
+                    TbFeet.Text = Convert.Distance(value, Distance.Centimeter, Distance.Feet).ToString("F4");
+                    TbYard.Text = Convert.Distance(value, Distance.Centimeter, Distance.Yard).ToString("F4");
+                    TbMile.Text = Convert.Distance(value, Distance.Centimeter, Distance.Mile).ToString("F4");
+                }
+                if (sender == TbMeter)
+                {
+                    TbMillimeter.Text = Convert.Distance(value, Distance.Meter, Distance.Millimeter).ToString("F4");
+                    TbCentimeter.Text = Convert.Distance(value, Distance.Meter, Distance.Centimeter).ToString("F4");
+                    TbKilometer.Text = Convert.Distance(value, Distance.Meter, Distance.Kilometer).ToString("F4");
+                    TbInch.Text = Convert.Distance(value, Distance.Meter, Distance.Inch).ToString("F4");
+                    TbFeet.Text = Convert.Distance(value, Distance.Meter, Distance.Feet).ToString("F4");
+                    TbYard.Text = Convert.Distance(value, Distance.Meter, Distance.Yard).ToString("F4");
+                    TbMile.Text = Convert.Distance(value, Distance.Meter, Distance.Mile).ToString("F4");
+                }
+                if (sender == TbKilometer)
+                {
+                    TbMillimeter.Text = Convert.Distance(value, Distance.Kilometer, Distance.Millimeter).ToString("F4");
+                    TbCentimeter.Text = Convert.Distance(value, Distance.Kilometer, Distance.Centimeter).ToString("F4");
+                    TbMeter.Text = Convert.Distance(value, Distance.Kilometer, Distance.Meter).ToString("F4");
+                    TbInch.Text = Convert.Distance(value, Distance.Kilometer, Distance.Inch).ToString("F4");
+                    TbFeet.Text = Convert.Distance(value, Distance.Kilometer, Distance.Feet).ToString("F4");
+                    TbYard.Text = Convert.Distance(value, Distance.Kilometer, Distance.Yard).ToString("F4");
+                    TbMile.Text = Convert.Distance(value, Distance.Kilometer, Distance.Mile).ToString("F4");
+                }
+                if (sender == TbInch)
+                {
+                    TbMillimeter.Text = Convert.Distance(value, Distance.Inch, Distance.Millimeter).ToString("F4");
+                    TbCentimeter.Text = Convert.Distance(value, Distance.Inch, Distance.Centimeter).ToString("F4");
+                    TbMeter.Text = Convert.Distance(value, Distance.Inch, Distance.Meter).ToString("F4");
+                    TbKilometer.Text = Convert.Distance(value, Distance.Inch, Distance.Kilometer).ToString("F4");
+                    TbFeet.Text = Convert.Distance(value, Distance.Inch, Distance.Feet).ToString("F4");
+                    TbYard.Text = Convert.Distance(value, Distance.Inch, Distance.Yard).ToString("F4");
+                    TbMile.Text = Convert.Distance(value, Distance.Inch, Distance.Mile).ToString("F4");
+                }
+                if (sender == TbFeet)
+                {
+                    TbMillimeter.Text = Convert.Distance(value, Distance.Feet, Distance.Millimeter).ToString("F4");
+                    TbCentimeter.Text = Convert.Distance(value, Distance.Feet, Distance.Centimeter).ToString("F4");
+                    TbMeter.Text = Convert.Distance(value, Distance.Feet, Distance.Meter).ToString("F4");
+                    TbKilometer.Text = Convert.Distance(value, Distance.Feet, Distance.Kilometer).ToString("F4");
+                    TbInch.Text = Convert.Distance(value, Distance.Feet, Distance.Inch).ToString("F4");
+                    TbYard.Text = Convert.Distance(value, Distance.Feet, Distance.Yard).ToString("F4");
+                    TbMile.Text = Convert.Distance(value, Distance.Feet, Distance.Mile).ToString("F4");
+                }
+                if (sender == TbYard)
+                {
+                    TbMillimeter.Text = Convert.Distance(value, Distance.Yard, Distance.Millimeter).ToString("F4");
+                    TbCentimeter.Text = Convert.Distance(value, Distance.Yard, Distance.Centimeter).ToString("F4");
+                    TbMeter.Text = Convert.Distance(value, Distance.Yard, Distance.Meter).ToString("F4");
+                    TbKilometer.Text = Convert.Distance(value, Distance.Yard, Distance.Kilometer).ToString("F4");
+                    TbInch.Text = Convert.Distance(value, Distance.Yard, Distance.Inch).ToString("F4");
+                    TbFeet.Text = Convert.Distance(value, Distance.Yard, Distance.Feet).ToString("F4");
+                    TbMile.Text = Convert.Distance(value, Distance.Yard, Distance.Mile).ToString("F4");
+                }
+                if (sender == TbMile)
+                {
+                    TbMillimeter.Text = Convert.Distance(value, Distance.Mile, Distance.Millimeter).ToString("F4");
+                    TbCentimeter.Text = Convert.Distance(value, Distance.Mile, Distance.Centimeter).ToString("F4");
+                    TbMeter.Text = Convert.Distance(value, Distance.Mile, Distance.Meter).ToString("F4");
+                    TbKilometer.Text = Convert.Distance(value, Distance.Mile, Distance.Kilometer).ToString("F4");
+                    TbInch.Text = Convert.Distance(value, Distance.Mile, Distance.Inch).ToString("F4");
+                    TbFeet.Text = Convert.Distance(value, Distance.Mile, Distance.Feet).ToString("F4");
+                    TbYard.Text = Convert.Distance(value, Distance.Mile, Distance.Yard).ToString("F4");
+                }
+            }
+            else
+            {
+                if (obj.Text == String.Empty)
+                {
+                    return;
+                }
+
+                TxBlMessageBlock.Text = "Geben Sie eine Zahl ein! Format: xx,xx";
+            }
+        }
+
+
+        private void CalculateTemperature(object sender, KeyEventArgs e)
+        {
+            TextBox obj = sender as TextBox;
+            decimal value = 0.0m;
+
+            TxBlMessageBlock.Text = String.Empty;
+            if (sender != TbKelvin) TbKelvin.Text = String.Empty;
+            if (sender != TbCelsius) TbCelsius.Text = String.Empty;
+            if (sender != TbFahrenheit) TbFahrenheit.Text = String.Empty;
+
+            if (decimal.TryParse(obj.Text, out value))
+            {
+                if(sender == TbKelvin)
+                {
+                    TbCelsius.Text = Convert.Temperature(value, Temperature.Kelvin, Temperature.Celsius).ToString("F4");
+                    TbFahrenheit.Text = Convert.Temperature(value, Temperature.Kelvin, Temperature.Fahrenheit).ToString("F4");
+                }
+                else if(sender == TbCelsius)
+                {
+                    TbKelvin.Text = Convert.Temperature(value, Temperature.Celsius, Temperature.Kelvin).ToString("F4");
+                    TbFahrenheit.Text = Convert.Temperature(value, Temperature.Celsius, Temperature.Fahrenheit).ToString("F4");
+                }
+                else if(sender == TbFahrenheit)
+                {
+                    TbKelvin.Text = Convert.Temperature(value, Temperature.Fahrenheit, Temperature.Kelvin).ToString("F4");
+                    TbCelsius.Text = Convert.Temperature(value, Temperature.Fahrenheit, Temperature.Celsius).ToString("F4");
+                }
+            }
+            else
+            {
+                if (obj.Text == String.Empty)
+                {
+                    return;
+                }
+
+                TxBlMessageBlock.Text = "Geben Sie eine Zahl ein! Format: xx,xx";
+            }
+        }
+    }
+}
